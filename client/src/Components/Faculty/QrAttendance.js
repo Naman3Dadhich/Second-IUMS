154ae1fcea90e28ea5ce5117b59d3b1749552c25
Naman3Dadhich <naman.21cse250@rtu.ac.in>
2023-08-20 11:43:52 +0530

    qr add button

--- client/src/Components/Faculty/QrAttendance.js
+++ client/src/Components/Faculty/QrAttendance.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 const QrAttendance = ({
-  admissionYear,
   facultyName,
   subject,
   branch,
@@ -10,10 +10,53 @@ const QrAttendance = ({
   semester,
 }) => {
   const [qr, setQr] = useState(null);
+
+  // /markAttendance/faculty/QR/markingAbsent
+
+  const admissionYear = moment()
+    .subtract(semester * 6, "months")
+    .format("yy");
+
+  const academicSession = admissionYear[2] + admissionYear[3];
+  const semesterIncrement = Math.floor((semester - 1) / 2); // Calculate how many times academicSession should be incremented
+  const incrementedAcademicSession = String(
+    parseInt(academicSession) + semesterIncrement
+  );
+  console.log("acad", incrementedAcademicSession);
+
+  async function CompleteAtd() {
+    const inputData = {
+      attendanceData: {
+        facultyName,
+        subject,
+        branch,
+        section,
+        batch,
+        semester,
+        academicSession: incrementedAcademicSession,
+      },
+    };
+    const res = await fetch("/markAttendance/faculty/QR/markingAbsent", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputData),
+    });
+
+    const data = await res.json();
+
+    if (
+      data.message === "Attendance marked successfully" ||
+      data.message === "Attendance Marked Successfully for new Semester"
+    ) {
+      window.alert("attendance marked succesfully");
+    }
+  }
+
   useEffect(() => {
     async function fetchData(inputData) {
       try {
-        console.log("chalja");
         const inputData = {
           attendanceData: {
             facultyName,
@@ -22,11 +65,12 @@ const QrAttendance = ({
             section,
             batch,
             semester,
-            academicSession: admissionYear,
+            academicSession: incrementedAcademicSession,
           },
         };
+        console.log(inputData);
 
-        const response = await fetch("/abcd", {
+        const response = await fetch("https://testing-iums.onrender.com/abcd", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -52,19 +96,29 @@ const QrAttendance = ({
   }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        flexDirection: "column",
-        minHeight: 60 + "vh",
-        alignItems: "center",
-      }}
-    >
-      <h1>Mark Your Attendance, For This class , using the given qr code.</h1>
-      {qr !== null ? <img src={qr} alt="qr"></img> : <h1>Loading... Qr..</h1>}
-      <p>Faculty can drag the qr image to new tab , for better experience</p>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexDirection: "column",
+          minHeight: 60 + "vh",
+          alignItems: "center",
+        }}
+      >
+        <h1>Mark Your Attendance, For This class , using the given qr code.</h1>
+        {qr !== null ? <img src={qr} alt="qr"></img> : <h1>Loading... Qr..</h1>}
+        <p>Faculty can drag the qr image to new tab , for better experience</p>
+      </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          CompleteAtd();
+        }}
+      >
+        Complete Attendance
+      </button>
+    </>
   );
 };
 
